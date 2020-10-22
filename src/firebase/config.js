09faf2a8326c0aaee5069e84af4db860940b3382
MizanurRahmann <mr926560@gmail.com>
2020-10-22 23:41:58 +0600

    Setup firebase.

---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const config = {
+    apiKey: "AIzaSyAJJCNsbhDeWpAxY9GK2_2ZzqHNOJhYnIk",
+    authDomain: "tap-chat-3b13c.firebaseapp.com",
+    databaseURL: "https://tap-chat-3b13c.firebaseio.com",
+    projectId: "tap-chat-3b13c",
+    storageBucket: "tap-chat-3b13c.appspot.com",
+    messagingSenderId: "882547081700",
+    appId: "1:882547081700:web:c4b4c62bf99e0c4a0087f1",
+    measurementId: "G-C5P669Q3LX"
+};
+  
+export default config;
