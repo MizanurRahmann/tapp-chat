1a3fa8014d6655760b48fd0ec2085b81efee40c0
MizanurRahmann <mr926560@gmail.com>
2020-10-25 12:31:53 +0600

    UPDATE: auth ui.

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../../styles/css/Auth.css';
+import imageLink from '../../styles/res/image/inbox.svg';
+
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    //Login logic
+    const login = event => {
+        event.preventDefault()
+        if(checkForm()){
+            
+        }
+    }
+
+    // Form validation function
+    const checkForm = () => {
+        if(!email || !password){
+            setError('You have to fill all fields.');
+            return false;
+        }
+        return true;
+    }
+
+
     return (
-        <div>
-            Login
+        <div className="auth">
+            <div className="auth_image">
+                <img src={imageLink} width="100%" alt="Join with us"/>
+            </div>
+            <div className="auth_content">
+                <form>
+                    <h2>Log in</h2>
+                    {/* input div for email */}
+                    <div className="input_div">
+                        <div className="i">
+                            <i className="far fa-envelope"></i>
+                        </div>
+                        <div className="inputDiv">
+                            <input
+                                type="email"
+                                className="input"
+                                placeholder="Email"
+                                value={email}
+                                onChange={event => setEmail(event.target.value)}
+                            />
+                        </div>
+                    </div>
+
+                    {/* input div for password */}
+                    <div className="input_div">
+                        <div className="i">
+                            <i className="fas fa-lock"></i>
+                        </div>
+                        <div className="inputDiv">
+                            <input
+                                type="password"
+                                className="input"
+                                placeholder="Password"
+                                value={password}
+                                onChange={event => setPassword(event.target.value)}
+                            />
+                        </div>
+                    </div>
+                    <small className="textError">{error}</small>
+                    <input type="submit" className="btn__submit" value="Login" onClick={login} />
+                    <div className="signup__link">
+                        Don't have any account? <Link to="/register">Register here</Link>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,31 +1,63 @@
 import React, { useState } from 'react';
-import './Auth.css';
+import { Link } from 'react-router-dom';
+import '../../styles/css/Auth.css';
+import imageLink from '../../styles/res/image/friends.svg';
 
 function Register() {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
 
     //Registration logic
     const register = event => {
-
+        event.preventDefault()
+        if(checkForm()){
+            console.log(email);
+        }
     }
+
+    // Form validation function
+    const checkForm = () => {
+        if(formEmpty()){
+            setError('You have to fill all fields.');
+            return false;
+        } else if(checkPassword()){
+            setError('Password shoud be matched.');
+            return false;
+        }
+        return true;
+    }
+
+    // Check for empty input field
+    const formEmpty = () => {
+        return !username ||
+               !email ||
+               !password ||
+               !confirmPassword;
+    }
+
+    // Check for password match
+    const checkPassword = () => {
+        return password !== confirmPassword
+    }
+
 
     return (
         <div className="auth">
             <div className="auth_image">
-
+                <img src={imageLink} width="100%" alt="Join with us"/>
             </div>
             <div className="auth_content">
                 <form>
-                    <h2>Start your Journey</h2>
+                    <h2>Lets study to</h2>
                     {/* input div for username */}
                     <div className="input_div">
                         <div className="i">
                             <i className="fas fa-user"></i>
                         </div>
-                        <div>
+                        <div className="inputDiv">
                             <input
                                 type="text"
                                 className="input"
@@ -41,7 +73,7 @@ function Register() {
                         <div className="i">
                             <i className="far fa-envelope"></i>
                         </div>
-                        <div>
+                        <div className="inputDiv">
                             <input
                                 type="email"
                                 className="input"
@@ -57,7 +89,7 @@ function Register() {
                         <div className="i">
                             <i className="fas fa-lock"></i>
                         </div>
-                        <div>
+                        <div className="inputDiv">
                             <input
                                 type="password"
                                 className="input"
@@ -73,7 +105,7 @@ function Register() {
                         <div className="i">
                             <i className="fas fa-lock"></i>
                         </div>
-                        <div>
+                        <div className="inputDiv">
                             <input
                                 type="password"
                                 className="input"
@@ -83,12 +115,14 @@ function Register() {
                             />
                         </div>
                     </div>
+                    
+                    <small className="textError">{error}</small>
 
-                    <input type="submit" className="btn__submit" value="Register" onClick={register} />
-
+                    {/* <input type="submit" className="btn__submit" value="Register" onClick={register} /> */}
+                    <button className="btn__submit" onClick={register}>Sign up</button>
                     <div className="signup__link">
-                        Already Have an account? Login here.
-            </div>
+                        Already Have an account? <Link to="/login">Login here</Link>
+                    </div>
                 </form>
             </div>
         </div>
